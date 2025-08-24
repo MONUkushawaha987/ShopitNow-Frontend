1ae43806f4cfa08a6c8a943680b718ed1d0b716e
Monu Kushawaha <91782501+MONUkushawaha987@users.noreply.github.com>
2025-08-24 23:03:18 +0530

    api.js file changes1

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 // const API_URL = process.env.API_URL ;
-const API = axios.create({ baseURL: process.env.API_URL || "http://localhost:5000" });
+const API = axios.create({ baseURL: process.env.API_URL });
 
 export const setAuthToken = (token) => {
   if (token) API.defaults.headers.common["Authorization"] = `Bearer ${token}`;
