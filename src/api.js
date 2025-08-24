efba4b35a714d4edc6556957fe43f6e20e3d3116
Monu Kushawaha <91782501+MONUkushawaha987@users.noreply.github.com>
2025-08-24 23:08:15 +0530

    api.js file changes2

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 // const API_URL = process.env.API_URL ;
-const API = axios.create({ baseURL: process.env.API_URL });
+const API = axios.create({ baseURL: "https://shopitnow-backend.onrender.com" });
 
 export const setAuthToken = (token) => {
   if (token) API.defaults.headers.common["Authorization"] = `Bearer ${token}`;
